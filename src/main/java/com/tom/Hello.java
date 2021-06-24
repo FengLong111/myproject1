55e019cdefdd2bfca2f0c28fbd48b4e5a499eb26
林品伸 <q22625931@gmail.com>
2021-06-24 09:35:50 +0800

    java and kotlin variable , data type

---
+++ src/main/java/com/tom/Hello.java
@@ -0,0 +1,19 @@
+package com.tom;
+
+public class Hello {
+    public static void main(String[] args) {
+//      System.out.println("Hello world");
+            Person p = new Person();
+            p.hello();
+            int age;
+            age = 21;
+            char c = '中';
+            byte b = 30;
+            float weight = 42.5f;
+            boolean enroll = true;
+            String s = new String("ABCDE");
+            System.out.println(enroll);
+
+
+    }
+}
---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,21 @@
+package com.tom
+
+import java.sql.SQLOutput
+
+fun main() {
+//   println("Hello kotlin")
+//   human().hello()
+        val h = human()
+        h.hello()
+        var age : Int = 21;
+        age = 22
+        val weight = 42
+        val enroll : Boolean = true
+        println(age)
+
+}
+class human {
+    fun hello(){
+        println("Hello kotlin")
+    }
+}
